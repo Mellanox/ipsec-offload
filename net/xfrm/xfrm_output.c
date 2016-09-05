022699d0c297c396011cc866f0df254e64a29778
Ilan Tayari <ilant@mellanox.com>
2016-09-05 08:28:26 +0300

    xfrm: Add encapsulation header offsets while SKB is not encrypted
    Both esp4 and esp6 used to assume that the SKB payload is encrypted
    and therefore the inner_network and inner_transport offsets are
    not relevant.
    When doing crypto offload in the NIC, this is no longer the case
    and the NIC driver needs these offsets so it can do TX TCP checksum
    offloading.
    This patch sets the inner_network and inner_transport members of
    the SKB, as well as encapsulation, to reflect the actual positions
    of these headers, and removes them only once encryption is done
    on the payload.
    
    Signed-off-by: Ilan Tayari <ilant@mellanox.com>
    Issue: None
    Change-Id: I5cbcc7341c4723c30f3d7dd958d083aee7cc81eb

--- net/xfrm/xfrm_output.c
+++ net/xfrm/xfrm_output.c
@@ -219,6 +219,7 @@ int xfrm_output(struct sock *sk, struct sk_buff *skb)
 		if (skb->sp)
 			secpath_put(skb->sp);
 		skb->sp = sp;
+		skb->encapsulation = 1;
 
 		sp->olen++;
 		sp->xvec[skb->sp->len++] = x;
@@ -232,6 +233,8 @@ int xfrm_output(struct sock *sk, struct sk_buff *skb)
 
 		if (x->xso.dev && x->xso.dev->features & NETIF_F_HW_ESP_TX_CSUM)
 			goto out;
+	} else {
+		skb->encapsulation = 0;
 	}
 
 	if (skb_is_gso(skb))
